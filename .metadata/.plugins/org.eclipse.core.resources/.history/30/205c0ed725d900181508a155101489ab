db8ed8b9e40cde7c328d88ca4def19ab9cdf0f57
Shanmuganathan <Preethi.Shanmuganathan@aig.com>
2018-11-01 15:52:18 +0530

    first commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/30/205c0ed725d900181508a155101489ab
@@ -0,0 +1,15 @@
+package in.javadomain.SpringBootHelloworld;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Hellocontroller {
+	
+	@RequestMapping("/hello")
+	public String hello() {
+		// TODO Auto-generated method stub
+		return "newhtml";
+	}
+
+}
---
+++ SpringBoothello/src/main/java/com/javadomain/SpringBoothello/SpringBoothelloApplication.java
@@ -0,0 +1,12 @@
+package com.javadomain.SpringBoothello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoothelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoothelloApplication.class, args);
+	}
+}
